91313d7f3d53c05106ce6fcca88215486aede31e
Dmitriy Udovik <du@nuvoapp.co>
2024-06-07 01:15:27 +0400

    Fixes v3

--- store/store.ts
+++ store/store.ts
@@ -1,21 +1,11 @@
 import { combineReducers, configureStore  } from "@reduxjs/toolkit";
 import {persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 
 import priceSlice from "@/store/features/priceSlice";
 
 
-const persistConfigParams = {
-	key: 'root',
-	version: 1,
-	storage,
-}
 
-const persistConfigId = {
-	key: 'id',
-	version: 2,
-	storage,
-}
 
 const rootReducer = combineReducers({
 	price: priceSlice
